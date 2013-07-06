c584c61795747136be629634efa084f9654ad062
Woonsan Ko <woonsan.ko@bloomreach.com>
2013-07-06 00:07:28 +0000

    Setting default cmis source repository module configuration

--- src/main/java/org/onehippo/forge/cmisreplication/CmisDocumentsReplicator.java
+++ src/main/java/org/onehippo/forge/cmisreplication/CmisDocumentsReplicator.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2011 Hippo.
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,19 +48,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CmisDocumentsReplicator {
-    
+
     private static Logger log = LoggerFactory.getLogger(CmisDocumentsReplicator.class);
-    
+
     private javax.jcr.Session jcrSession;
     private CmisRepoConfig cmisRepoConfig;
     private HippoRepoConfig hippoRepoConfig;
     private boolean updateCmisDocumentsToRepository;
     private boolean updateRepositoryDocumentsToCmis;
-    
+
     public void setJcrSession(javax.jcr.Session jcrSession) {
         this.jcrSession = jcrSession;
     }
-    
+
     public CmisRepoConfig getCmisRepoConfig() {
         return cmisRepoConfig;
     }
@@ -68,7 +68,7 @@ public class CmisDocumentsReplicator {
     public void setCmisRepoConfig(CmisRepoConfig cmisRepoConfig) {
         this.cmisRepoConfig = cmisRepoConfig;
     }
-    
+
     public HippoRepoConfig getHippoRepoConfig() {
         return hippoRepoConfig;
     }
@@ -76,7 +76,7 @@ public class CmisDocumentsReplicator {
     public void setHippoRepoConfig(HippoRepoConfig hippoRepoConfig) {
         this.hippoRepoConfig = hippoRepoConfig;
     }
-    
+
     public boolean isUpdateCmisDocumentsToRepository() {
         return updateCmisDocumentsToRepository;
     }
@@ -95,62 +95,62 @@ public class CmisDocumentsReplicator {
 
     public void execute() {
         log.debug("Executing Cmis Documents Replicator ...");
-        
+
         if (updateCmisDocumentsToRepository) {
             try {
-                updateCmisDocumentsToRepository();
+                updateCmisDocumentsToHippoRepository();
             } catch (Exception e) {
-                log.warn("Failed to update Cmis Documents To Repository. {}", e.toString());
+                log.warn("Failed to update Cmis Documents To Hippo Repository: " + e, e);
             }
         }
-        
+
         if (updateRepositoryDocumentsToCmis) {
             try {
-                updateRepositoryDocumentsToCmis();
+                updateHippoRepositoryDocumentsToCmis();
             } catch (Exception e) {
-                log.warn("Failed to update Repository Documents To Cmis. {}", e.toString());
+                log.warn("Failed to update Repository Documents To CMIS Repository: " + e, e);
             }
         }
     }
-    
-    private void updateCmisDocumentsToRepository() throws RepositoryException, IOException {
+
+    private void updateCmisDocumentsToHippoRepository() throws RepositoryException, IOException {
         Session session = null;
-        
+
         try {
             session = createSession();
             OperationContext operationContext = session.createOperationContext();
             operationContext.setMaxItemsPerPage(cmisRepoConfig.getMaxItemsPerPage());
-            
+
             CmisObject seed = null;
-            
+
             if (StringUtils.isBlank(cmisRepoConfig.getRootPath())) {
                 seed = session.getRootFolder(operationContext);
             } else {
                 seed = session.getObjectByPath(cmisRepoConfig.getRootPath(), operationContext);
             }
-            
+
             List<String> documentIds = new LinkedList<String>();
             fillAllDocumentIdsFromCMIS(seed, documentIds);
-            
+
             for (String documentId : documentIds) {
                 Document document = null;
-                
+
                 try {
                     document = (Document) session.getObject(documentId);
                 } catch (CmisObjectNotFoundException ignore) {
                 }
-                
+
                 if (document == null) {
                     continue;
                 }
-                
+
                 //log.debug("Processing document: " + document.getPaths());
-                String assetPath = 
-                    hippoRepoConfig.getRootPath() + "/" + 
+                String assetPath =
+                    hippoRepoConfig.getRootPath() + "/" +
                     StringUtils.removeStart(StringUtils.removeStart(document.getPaths().get(0), cmisRepoConfig.getRootPath()), "/");
                 AssetMetadata metadata = AssetUtils.getAssetMetadata(jcrSession, assetPath);
                 long documentLastModified = document.getLastModificationDate().getTimeInMillis();
-                
+
                 if (metadata == null || documentLastModified > metadata.getLastModified()) {
                     int offset = assetPath.lastIndexOf("/");
                     String assetFolderPath = assetPath.substring(0, offset);
@@ -170,33 +170,33 @@ public class CmisDocumentsReplicator {
             }
         }
     }
-    
-    private void updateRepositoryDocumentsToCmis() throws RepositoryException, IOException {
+
+    private void updateHippoRepositoryDocumentsToCmis() throws RepositoryException, IOException {
         Session session = null;
-        
+
         try {
             session = createSession();
             OperationContext operationContext = session.createOperationContext();
             operationContext.setMaxItemsPerPage(cmisRepoConfig.getMaxItemsPerPage());
-            
+
             Node seed = null;
-            
+
             if (jcrSession.itemExists(hippoRepoConfig.getRootPath())) {
                 seed = jcrSession.getNode(hippoRepoConfig.getRootPath());
             }
-            
+
             if (seed != null) {
                 List<String> documentIds = new LinkedList<String>();
                 fillAllDocumentIdsFromRepository(seed, documentIds);
-                
+
                 for (String documentId : documentIds) {
                     Document document = null;
-                    
+
                     try {
                         document = (Document) session.getObject(documentId);
                     } catch (CmisObjectNotFoundException ignore) {
                     }
-                    
+
                     if (document == null) {
                         try {
                             removeAssetByDocumentId(jcrSession, documentId);
@@ -216,30 +216,30 @@ public class CmisDocumentsReplicator {
             }
         }
     }
-    
+
     private Session createSession() {
         Map<String, String> sessionParams = new HashMap<String, String>();
-        
+
         sessionParams.put(SessionParameter.USER, cmisRepoConfig.getUsername());
         sessionParams.put(SessionParameter.PASSWORD, cmisRepoConfig.getPassword());
         sessionParams.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
         sessionParams.put(SessionParameter.ATOMPUB_URL, cmisRepoConfig.getUrl());
         sessionParams.put(SessionParameter.REPOSITORY_ID, cmisRepoConfig.getRepositoryId());
-        
+
         SessionFactory factory = SessionFactoryImpl.newInstance();
         Session session = factory.createSession(sessionParams);
-        
+
         return session;
     }
-    
+
     private void fillAllDocumentIdsFromCMIS(CmisObject seed, List<String> documentIds) {
         String baseType = seed.getBaseType().getId();
-        
+
         if (ObjectType.DOCUMENT_BASETYPE_ID.equals(baseType)) {
             documentIds.add(seed.getId());
         } else if (ObjectType.FOLDER_BASETYPE_ID.equals(baseType)) {
             ItemIterable<CmisObject> children = ((Folder) seed).getChildren();
-            
+
             if (children.getTotalNumItems() > 0) {
                 for (CmisObject item : children) {
                     fillAllDocumentIdsFromCMIS(item, documentIds);
@@ -247,45 +247,45 @@ public class CmisDocumentsReplicator {
             }
         }
     }
-    
+
     private void fillAllDocumentIdsFromRepository(Node seed, List<String> documentIds) throws RepositoryException {
         if (seed.isNodeType(CmisReplicationTypes.HIPPO_HANDLE)) {
             seed = seed.getNode(seed.getName());
         }
-        
+
         if (seed.isNodeType(CmisReplicationTypes.EXAMPLE_ASSET_SET) && seed.hasProperty(CmisReplicationTypes.CMIS_OBJECT_ID)) {
             documentIds.add(seed.getProperty(CmisReplicationTypes.CMIS_OBJECT_ID).getString());
         } else if (seed.isNodeType(CmisReplicationTypes.HIPPO_ASSET_GALLERY)) {
             for (NodeIterator nodeIt = seed.getNodes(); nodeIt.hasNext(); ) {
                 Node child = nodeIt.nextNode();
-                
+
                 if (child != null) {
                     fillAllDocumentIdsFromRepository(child, documentIds);
                 }
             }
         }
     }
-    
+
     private void removeAssetByDocumentId(javax.jcr.Session jcrSession, String documentId) throws RepositoryException {
         QueryManager queryManager = jcrSession.getWorkspace().getQueryManager();
         String statement = "//element(*," + CmisReplicationTypes.EXAMPLE_ASSET_SET + ")[@" + CmisReplicationTypes.CMIS_OBJECT_ID +"='" + documentId + "']";
         Query query = queryManager.createQuery(statement, Query.XPATH);
         QueryResult result = query.execute();
         boolean removed = false;
-        
+
         for (NodeIterator nodeIt = result.getNodes(); nodeIt.hasNext(); ) {
             Node node = nodeIt.nextNode();
-            
+
             if (node != null) {
                 Node parentNode = node.getParent();
-                
+
                 if (parentNode.isNodeType(CmisReplicationTypes.HIPPO_HANDLE)) {
                     parentNode.remove();
                     removed = true;
                 }
             }
         }
-        
+
         if (removed) {
             jcrSession.save();
         }
