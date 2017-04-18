bd2879aef18fdbf5ab13ee4f57916d218098bb0a
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-04-18 18:32:39 -0400

    Adding demo and generated docs

---
+++ demo/site/src/main/java/org/onehippo/forge/cmisreplication/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.cmisreplication.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="cmisreplicationdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
